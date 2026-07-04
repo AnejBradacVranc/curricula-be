b2c151824fdebd7cf14a8f451bef8fc93450654d
AnejBradacVranc <mikedrifner@gmail.com>
2026-07-04 16:28:25 +0200

    Refactor import path for PrismaService in AssignmentsService to align with new directory structure.

--- src/modules/assignment/assignment.service.ts
+++ src/modules/assignment/assignment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SubjectTeacher } from 'generated/prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class AssignmentsService {
