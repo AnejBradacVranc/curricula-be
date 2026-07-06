e89b69f0cf359a362aa537b7515a8e8aeb3bc501
AnejBradacVranc <mikedrifner@gmail.com>
2026-07-06 21:49:26 +0200

    Remove unused DTO imports and comments in ProgramsController for cleaner code. Update return type to simplify response structure.

--- src/modules/program/program.controller.ts
+++ src/modules/program/program.controller.ts
@@ -3,8 +3,8 @@ import { Program } from 'generated/prisma/client';
 import { CreateProgramDto } from './dto/create-program.dto';
 import { ProgramsService, ProgramWithRelations } from './program.service';
 import { AuthenticatedUser } from '../auth/strategies/jwt.strategy';
-import { ProgramByClassDto } from './dto/program-by-class.dto';
-import { GetProgramsQueryDto } from './dto/get-program.dto';
+//import { ProgramByClassDto } from './dto/program-by-class.dto';
+//import { GetProgramsQueryDto } from './dto/get-program.dto';
 
 @Controller()
 export class ProgramsController {
@@ -14,7 +14,7 @@ export class ProgramsController {
   async getPrograms(
     @Request() req: { user: AuthenticatedUser },
     //@Query() params: GetProgramsQueryDto,
-  ): Promise<ProgramWithRelations[] | ProgramByClassDto[]> {
+  ): Promise<ProgramWithRelations[] /*| ProgramByClassDto[]*/> {
     return await this.programsService.programsBySchool(req.user.schoolId);
   }
 
